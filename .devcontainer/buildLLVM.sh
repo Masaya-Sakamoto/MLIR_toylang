1e514d42e45f8f0e198ab0f9f87501bf8b7dab37
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-10-17 10:57:41 +0900

    Refactor build scripts to accept LLVM version as an argument and streamline build process

--- .devcontainer/buildLLVM.sh
+++ .devcontainer/buildLLVM.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
+# get llvm version from input arguments
+# example: ./buildLLVM.sh -llvm_version 21.x
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -llvm_version)
+        LLVM_VERSION="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        shift # past argument
+        ;;
+    esac
+done
+
+echo "Building LLVM version: $LLVM_VERSION"
+
 # Build LLVM with MLIR and specific configurations
 # source: https://www.youtube.com/watch?v=KYaojNbujKM&list=PLlONLmJCfHTo9WYfsoQvwjsa5ZB6hjOG5&index=2
 
+CUSTOM_LLVM_INSTALL_PREFIX="/usr/local/llvm-$LLVM_VERSION"
+
 cmake -G Ninja ../llvm \
-   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DCMAKE_INSTALL_PREFIX=$CUSTOM_LLVM_INSTALL_PREFIX \
+   -DLLVM_PARALLES_COMPILE_JOBS=4 \
+   -DLLVM_PARALLEL_LINK_JOBS=1 \
    -DLLVM_BUILD_EXAMPLES=ON \
-   -DLLVM_TARGETS_TO_BUILD="Native" \
+   -DLLVM_TARGETS_TO_BUILD="x86_64" \
    -DCMAKE_BUILD_TYPE=Release \
    -DLLVM_ENABLE_ASSERTIONS=ON \
-   -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON \
    -DLLVM_CCACHE_BUILD=ON \
-   -DLLVM_USE_SANITIZER="Address;Undefined";
-   ninja;
+   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+   -DLLVM_ENABLE_PROJECTS='clang;lldb;lld;mlir;clang-tools-extra;compiler-rt' \
+   -DCMAKE_C_COMPILER=clang \
+   -DCMAKE_CXX_COMPILER=clang++ \
+   -DLLVM_ENABLE_LLD=ON
+
+cmake --build .
+cmake --build . --target check-mlir
+sudo cmake -DCMAKE_INSTALL_PREFIX=$CUSTOM_LLVM_INSTALL_PREFIX -P cmake_install.cmake
--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -6,7 +6,8 @@ LLVM_DIR='~/llvm-project'
 LLVM_BUILD_DIR="$LLVM_DIR/build"
 LLVM_BUILD_SCRIPT="$DEVCONTAINER_DIR/buildLLVM.sh"
 LLVM_REPO_URL="https://github.com/llvm/llvm-project.git"
-LLVM_CHECKOUT="release/21.x"
+LLVM_VERSION='21.x'
+LLVM_CHECKOUT="release/$LLVM_VERSION"
 
 # Clone LLVM Project if not already present or empty
 if [ ! -d "$LLVM_DIR" ] || [ -z "$(ls -A $LLVM_DIR)" ]; then
@@ -17,28 +18,28 @@ else
     git pull;
 fi
 
-# Build LLVM if not already built
+# Build & Install LLVM if not already built
 if [ ! -d "$LLVM_BUILD_DIR" ] || [ -z "$(ls -A $LLVM_BUILD_DIR)" ]; then
     mkdir -p $LLVM_BUILD_DIR;
     cd $LLVM_BUILD_DIR;
-    ${LLVM_BUILD_SCRIPT};
-    cd ../..;
+    ${LLVM_BUILD_SCRIPT} -llvm_version $LLVM_VERSION;
+    cd $WORKSPACE_DIR;
 fi
 
-# Add LLVM tools to PATH if not already present
-if [[ ":$PATH:" != *":$WORKSPACE_DIR/llvm-project/build/bin:"* ]]; then
-    echo "export PATH=\$PATH:$WORKSPACE_DIR/llvm-project/build/bin" >> ~/.bashrc;
-    export PATH=$PATH:$WORKSPACE_DIR/llvm-project/build/bin;
-fi
+# # Add LLVM tools to PATH if not already present
+# if [[ ":$PATH:" != *":$WORKSPACE_DIR/llvm-project/build/bin:"* ]]; then
+#     echo "export PATH=\$PATH:$WORKSPACE_DIR/llvm-project/build/bin" >> ~/.bashrc;
+#     export PATH=$PATH:$WORKSPACE_DIR/llvm-project/build/bin;
+# fi
 
-# Add LLVM libraries to LD_LIBRARY_PATH if not already present
-if [[ ":$LD_LIBRARY_PATH:" != *":$WORKSPACE_DIR/llvm-project/build/lib:"* ]]; then
-    echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$WORKSPACE_DIR/llvm-project/build/lib" >> ~/.bashrc;
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKSPACE_DIR/llvm-project/build/lib;
-fi
+# # Add LLVM libraries to LD_LIBRARY_PATH if not already present
+# if [[ ":$LD_LIBRARY_PATH:" != *":$WORKSPACE_DIR/llvm-project/build/lib:"* ]]; then
+#     echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$WORKSPACE_DIR/llvm-project/build/lib" >> ~/.bashrc;
+#     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKSPACE_DIR/llvm-project/build/lib;
+# fi
 
-# Add LLVM headers to CPLUS_INCLUDE_PATH if not already present
-if [[ ":$CPLUS_INCLUDE_PATH:" != *":$WORKSPACE_DIR/llvm-project/build/include:"* ]]; then
-    echo "export CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH:$WORKSPACE_DIR/llvm-project/build/include" >> ~/.bashrc;
-    export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$WORKSPACE_DIR/llvm-project/build/include;
-fi
+# # Add LLVM headers to CPLUS_INCLUDE_PATH if not already present
+# if [[ ":$CPLUS_INCLUDE_PATH:" != *":$WORKSPACE_DIR/llvm-project/build/include:"* ]]; then
+#     echo "export CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH:$WORKSPACE_DIR/llvm-project/build/include" >> ~/.bashrc;
+#     export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$WORKSPACE_DIR/llvm-project/build/include;
+# fi
